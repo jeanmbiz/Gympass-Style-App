b8d94e7deae9866943880bd04707408d3766e33e
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-12 11:05:52 -0300

    feat: add register user use case

---
+++ src/use-cases/register-use-case.ts
@@ -0,0 +1,30 @@
+import { UsersRepositoryInterface } from '@/repositories/users-repository-interface'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  // construtor recebe as dependências - Inversão de Dependência
+  constructor(private usersRepository: UsersRepositoryInterface) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
