99ca76af047b86da6e6dabbfd8951d08e1aaa27e
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-13 07:50:56 -0300

    refactor: register use case

--- src/use-cases/register-use-case.ts
+++ src/use-cases/register-use-case.ts
@@ -1,6 +1,7 @@
-import { UsersRepositoryInterface } from '@/repositories/users-repository-interface'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { UsersRepository } from '@/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,11 +9,19 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   // construtor recebe as dependências - Inversão de Dependência
-  constructor(private usersRepository: UsersRepositoryInterface) {}
+  constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +30,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
