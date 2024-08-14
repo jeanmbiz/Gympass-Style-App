8d386a1e677cc00144be2812bc5027006211c474
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-14 08:08:37 -0300

    feat: add authenticate route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register-controller'
+import { authenticateController } from './controllers/authenticate-controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
+
+  // estou criando (post) uma sessão (/sessions) de login/autenticação
+  app.post('/sessions', authenticateController)
 }
