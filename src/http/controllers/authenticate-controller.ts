71f37501346b91bbbcd4c2a964f6a37f45ea6110
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-14 08:08:13 -0300

    feat: add factory and authenticate use controller

---
+++ src/http/controllers/authenticate-controller.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credetials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+
+export async function authenticateController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    // chama a factoru de AuthenticateUseCase
+    const authenticateUseCase = makeAuthenticateUseCase()
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+    throw error
+  }
+
+  return reply.status(200).send()
+}
