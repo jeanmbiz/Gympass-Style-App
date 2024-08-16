ca7c785b321881016ff3fcf4aa9fe8c65ba30e00
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-16 07:58:00 -0300

    feat: add gyms and in memory gyms repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,9 +1,28 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 import { GymsRepository } from '../gyms-repository'
+import { randomUUID } from 'crypto'
 
 export class InMemoryGymsRepository implements GymsRepository {
   // items representando a tabela de academias
   public items: Gym[] = []
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      // prisma não aceita undefined
+      Description: data.Description ?? null,
+      phone: data.phone ?? null,
+      // converter Latitude em formato aceto pelo prisma
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      Longitude: new Prisma.Decimal(data.Longitude.toString()),
+      created_at: new Date(),
+    }
+
+    this.items.push(gym)
+
+    return gym
+  }
 
   async findById(gymId: string) {
     const gym = this.items.find((item) => item.id === gymId)
