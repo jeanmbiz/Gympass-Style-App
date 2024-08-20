3d0399c09da8ba5ec93119aa63251313290509b8
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-20 08:00:09 -0300

    feat: add Validate Check In Use Case

---
+++ src/use-cases/validate-check-in-use-case.ts
@@ -0,0 +1,44 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    // retorna a diferença em 'minutes' da criação do checkIn
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
+    }
+
+    // salva data atual no campo validated_at
+    checkIn.validated_at = new Date()
+
+    // atualiza check-in no banco de dados
+    await this.checkInsRepository.save(checkIn)
+
+    return { checkIn }
+  }
+}
