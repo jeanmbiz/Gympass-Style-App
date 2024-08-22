b8dd2b2908cf9140719e9168d551ddba39561e95
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-22 08:29:43 -0300

    feat: add verifyJWT in authenticate route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register-controller'
 import { authenticateController } from './controllers/authenticate-controller'
 import { profileController } from './controllers/profile-controller'
+import { verifyJWT } from './middlewares/verify.jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
@@ -10,5 +11,6 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticateController)
 
   // rota buscar perfil autenticado
-  app.get('/me', profileController)
+  // onRequest: executa antes do controller, passando middleware de verificação de token
+  app.get('/me', { onRequest: [verifyJWT] }, profileController)
 }
