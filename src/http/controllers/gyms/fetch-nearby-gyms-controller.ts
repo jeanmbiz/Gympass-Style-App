34dc1d5009e265ffba44d42dbcde056832469b4d
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-27 09:12:58 -0300

    feat: add fetch nearby gyms controller

---
+++ src/http/controllers/gyms/fetch-nearby-gyms-controller.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function fetchNearbyGymsController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const fetchNearbyGymsSchema = z.object({
+    // refine para validação que não é padrão do zod
+    latitude: z.number().refine((value) => {
+      // validalçao de latitude ser <= +-90
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      // validalçao de latitude ser <= +-180
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = fetchNearbyGymsSchema.parse(request.body)
+
+  // chama a factory de RegisterUseCase
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  // pega retorno do caso de uso
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
