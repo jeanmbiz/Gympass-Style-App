296a83f47d402834d0a63fe71f0a491174d19a75
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-28 08:17:42 -0300

    feat: add check ins routes

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify.jwt'
+import { createCheckInController } from './create-check-in-controller'
+import { ValidateCheckInController } from './validate-check-in-controller'
+import { FetchUserCheckInsHistoryController } from './fetch-user-check-ins-history-controller'
+import { GetUserMetricsController } from './get-user-metrics-controller'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  // todas as rotas abaixo precisarão de autenticação verifyJWT
+  app.addHook('onRequest', verifyJWT)
+
+  app.post('/gyms/:gymId/check-ins', createCheckInController)
+
+  app.get('/check-ins/history', FetchUserCheckInsHistoryController)
+  app.get('/check-ins/metrics', GetUserMetricsController)
+
+  app.patch('/check-ins/:checkInId/validate', ValidateCheckInController)
+}
