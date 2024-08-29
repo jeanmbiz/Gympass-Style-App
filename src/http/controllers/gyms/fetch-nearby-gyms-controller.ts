803c3368f4f047ad215d4dcac95dc8dafeaeeb24
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-29 08:35:16 -0300

    refactor: schema validation and params of fetchNearbyGymsController

--- src/http/controllers/gyms/fetch-nearby-gyms-controller.ts
+++ src/http/controllers/gyms/fetch-nearby-gyms-controller.ts
@@ -9,18 +9,18 @@ export async function fetchNearbyGymsController(
   // valida parametros da query. ex: /search?query=javascript&sort=desc
   const fetchNearbyGymsQuerySchema = z.object({
     // refine para validação que não é padrão do zod
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       // validalçao de latitude ser <= +-90
       return Math.abs(value) <= 90
     }),
-    longitude: z.number().refine((value) => {
+    Longitude: z.coerce.number().refine((value) => {
       // validalçao de latitude ser <= +-180
       return Math.abs(value) <= 180
     }),
   })
 
   // busca parametros validados da query. ex: /search?query=javascript&sort=desc
-  const { latitude, longitude } = fetchNearbyGymsQuerySchema.parse(
+  const { latitude, Longitude } = fetchNearbyGymsQuerySchema.parse(
     request.query,
   )
 
@@ -30,7 +30,7 @@ export async function fetchNearbyGymsController(
   // pega retorno do caso de uso
   const { gyms } = await fetchNearbyGymsUseCase.execute({
     userLatitude: latitude,
-    userLongitude: longitude,
+    userLongitude: Longitude,
   })
 
   return reply.status(200).send({
