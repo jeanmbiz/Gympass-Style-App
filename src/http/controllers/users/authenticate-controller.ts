005ed753cffe50e7ce4edbab8e750d7cd09ad457
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-30 07:48:13 -0300

    feat: add refreshToken on authentication

--- src/http/controllers/users/authenticate-controller.ts
+++ src/http/controllers/users/authenticate-controller.ts
@@ -37,9 +37,35 @@ export async function authenticateController(
       },
     )
 
-    return reply.status(200).send({
-      token,
+    // criar refresh token
+    // usuário vai perder autenticação se ficar 7 dias sem entrar nela
+    const refreshToken = await reply.jwtSign({
+      sign: {
+        // sub: campo reservado no token JWT para guardar id do usuário
+        sub: user.id,
+        expiresIn: '7d', // expira em 7 dias
+      },
     })
+
+    return (
+      reply
+        // envia refreshToken através de cookie
+        .setCookie('refreshToken', refreshToken, {
+          // configurações de segurança do cookie
+          // path: todas as rotas back-end terão acesso ao cookie
+          path: '/',
+          // secure: cookie será encriptado atraves do https
+          secure: true,
+          // sameSite: cookie só sera acessível dentro do mesmo domínio
+          sameSite: true,
+          // httpOnly: cookie só será acessado atraves de req e res no Back-end, não será salvo no browser do front
+          httpOnly: true,
+        })
+        .status(200)
+        .send({
+          token,
+        })
+    )
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(409).send({ message: error.message })
