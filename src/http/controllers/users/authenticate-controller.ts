45159c18755260e54e60a6ffaa9f2691a0ecc563
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-31 11:41:17 -0300

    refactor: add user information on refresh token

--- src/http/controllers/users/authenticate-controller.ts
+++ src/http/controllers/users/authenticate-controller.ts
@@ -1,6 +1,6 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credetials-error'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticateController(
@@ -23,15 +23,13 @@ export async function authenticateController(
       password,
     })
 
-    // jwtSign: server para gerar token do usuário
+    // jwtSign: server para gerar token JWT do usuário
     const token = await reply.jwtSign(
       {
-        // guarda informações adicionais do usuário
         email: user.email,
       },
       {
         sign: {
-          // sub: campo reservado no token JWT para guardar id do usuário
           sub: user.id,
         },
       },
@@ -39,37 +37,49 @@ export async function authenticateController(
 
     // criar refresh token
     // usuário vai perder autenticação se ficar 7 dias sem entrar nela
-    const refreshToken = await reply.jwtSign({
-      sign: {
-        // sub: campo reservado no token JWT para guardar id do usuário
-        sub: user.id,
-        expiresIn: '7d', // expira em 7 dias
+    const refreshToken = await reply.jwtSign(
+      {
+        // guarda email do usuário no refresh token, para dps passar informação para JWT
+        email: user.email,
       },
-    })
+      {
+        sign: {
+          // sub: campo reservado no token JWT para guardar id do usuário
+          sub: user.id,
+          expiresIn: '7d', // expira em 7 dias
+        },
+      },
+    )
 
     return (
       reply
         // envia refreshToken através de cookie
-        .setCookie('refreshToken', refreshToken, {
+        .setCookie(
+          'refreshToken',
+          refreshToken,
           // configurações de segurança do cookie
-          // path: todas as rotas back-end terão acesso ao cookie
-          path: '/',
-          // secure: cookie será encriptado atraves do https
-          secure: true,
-          // sameSite: cookie só sera acessível dentro do mesmo domínio
-          sameSite: true,
-          // httpOnly: cookie só será acessado atraves de req e res no Back-end, não será salvo no browser do front
-          httpOnly: true,
-        })
+          {
+            // path: todas as rotas back-end terão acesso ao cookie
+            path: '/',
+            // secure: cookie será encriptado atraves do https
+            secure: true,
+            // sameSite: cookie só sera acessível dentro do mesmo domínio
+            sameSite: true,
+            // httpOnly: cookie só será acessado atraves de req e res no Back-end, não será salvo no browser do front
+            httpOnly: true,
+          },
+        )
         .status(200)
         .send({
           token,
         })
     )
   } catch (error) {
+    console.log('error', error)
     if (error instanceof InvalidCredentialsError) {
-      return reply.status(409).send({ message: error.message })
+      return reply.status(400).send({ message: error.message })
     }
+
     throw error
   }
 }
