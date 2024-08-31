e31d133da3c675c4f7d332bc86f8ef76680d9239
jeanmbiz <jeanmbiz@hotmail.com>
2024-08-31 11:47:07 -0300

    feat: add refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,12 +5,16 @@ import { verifyJWT } from '@/http/middlewares/verify.jwt'
 import { registerController } from './register-controller'
 import { authenticateController } from './authenticate-controller'
 import { profileController } from './profile-controller'
+import { refreshTokenController } from './refresh-token-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
 
   // estou criando um (post), da rota (/sessions) de autenticação controller
   app.post('/sessions', authenticateController)
+
+  // rota para quando o refresh token nao for mais válido
+  app.patch('/token/refresh', refreshTokenController)
 
   // rota buscar perfil autenticado
   // onRequest: executa antes do controller, passando middleware de verificação de token
