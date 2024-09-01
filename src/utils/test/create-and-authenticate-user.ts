143260b9014ae6839481bc4240a8fe7cb8b31e86
jeanmbiz <jeanmbiz@hotmail.com>
2024-09-01 11:43:38 -0300

    refactor: to be possible create an user as admin or member

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,13 +1,22 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
 // app é uma instancia do Fastify, que será passada ao chamar função
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  // request para criar novo usuário
-  await request(app.server).post('/users').send({
-    name: 'User',
-    email: 'email@email.com.br',
-    password: '123456',
+// recebe como parâmetro se é admin, padrão false
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  // criar usuário pelo prisma, passando se é admin
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'email@email.com.br',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   // request para logar usuário
